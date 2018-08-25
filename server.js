6a9f78f143bc61704ee68354c97d08206bff4406
axke <ackempf@eagles.usi.edu>
2018-08-24 22:13:33 -0500

    bug(server): issue with path

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ if (process.env.NODE_ENV === 'prod') {
   // if not development, run the ng app
   app.use(express.static(`${__dirname}/dist`));
   app.get(/^\/(?!api).*/, (req, res) => {
-    res.sendFile(path.join(`${__dirname}/dist/index.html`));
+    res.sendFile(`${__dirname}/dist/index.html`);
   });
 }
 
