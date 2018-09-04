34f84fbd6c1036cdcb0db2283f9a4245de499fb4
axke <ackempf@eagles.usi.edu>
2018-09-04 12:51:49 -0500

    feat(stats): added region select

--- src/app/pages/player/stats/stats.component.ts
+++ src/app/pages/player/stats/stats.component.ts
@@ -8,12 +8,18 @@ import {Component, Input, OnInit} from '@angular/core';
 export class StatsComponent implements OnInit {
   @Input() player: any;
   @Input() seasons: any;
+  region: string;
 
   constructor() {
   }
 
   ngOnInit() {
+    this.region = 'ncsa';
     console.log('player', this.player);
+  }
+
+  changeRegion(region: string) {
+    this.region = region;
   }
 
 }
